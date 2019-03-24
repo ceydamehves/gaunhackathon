74571805d2ba8f31d9c1e6b6909125510184ff34
Halil İbrahim ceylan <hibrahim.ceylan@std.hku.edu.tr>
2019-03-24 09:57:49 +0300

    navigator

--- navigator/AppNavigator.js
+++ navigator/AppNavigator.js
@@ -1,23 +1,34 @@
 import {createStackNavigator, createAppContainer} from 'react-navigation';
 import HomeScreen from '../screens/MainScreen.js';
 import EmojiScreen from '../screens/EmojiScreen.js';
+import EmotionScreen from '../screens/EmotionSearch.js';
 import ThanksScreen from '../screens/ThanksScreen.js';
-const AppNavigator = createStackNavigator ({
-  Home: {
-    screen: HomeScreen,
+import PlaceScreen from '../screens/PlaceScreen.js';
+const AppNavigator = createStackNavigator (
+  {
+    Home: {
+      screen: HomeScreen,
+    },
+    EmojiScreen: {
+      screen: EmojiScreen,
+    },
+    EmotionSearch: {
+      screen: EmotionScreen,
+    },
+    ThanksScreen: {
+      screen: ThanksScreen,
+    },
+    PlaceScreen: {
+      screen: PlaceScreen,
+    },
   },
-  EmojiScreen: {
-    screen: EmojiScreen,
-  },
-  ThanksScreen: {
-    screen: ThanksScreen,
-  },
-},{
-  initialRouteName: 'EmojiScreen',
-  headerMode: 'none',
-  navigationOptions: {
-    headerVisible: false,
-  },
-});
+  {
+    initialRouteName: 'EmojiScreen',
+    headerMode: 'none',
+    navigationOptions: {
+      headerVisible: false,
+    },
+  }
+);
 
 export default createAppContainer (AppNavigator);
